3075b20933064e5b414e07b7cb2354bcbc7b69ce
liza63252 <lizkasuv@gmail.com>
2025-03-11 18:34:39 +0200

    Update 99_homework_template.py
    informatika

--- 06_2025-02-14_Algoritmų_parinkimas_(cikliniai_algoritmai)/99_homework_template.py
+++ 06_2025-02-14_Algoritmų_parinkimas_(cikliniai_algoritmai)/99_homework_template.py
@@ -1,13 +1,17 @@
 def find_intersection(list1, list2):
-    #Функция находит пересечение двух массивов с помощью двойного цикла
-    return [0, 0, 0]
+    intersection = []#Функция находит пересечение двух массивов с помощью двойного цикла
+    for num1 in arr1:
+        for num2 in arr2:
+            if num1 == num2 and num1 not in intersection:
+                intersection.append(num1)
+    return intersection            
 
 # Ввод данных
-list1 = list(map(int, input("Введите элементы первого массива через пробел: ").split()))
-list2 = list(map(int, input("Введите элементы второго массива через пробел: ").split()))
+arr1 = list(map(int, input("Введите элементы первого массива через пробел: ").split()))
+arr2 = list(map(int, input("Введите элементы второго массива через пробел: ").split()))
 
 # Вызов функции
-intersection = find_intersection(list1, list2)
+result = find_intersection(arr1, arr2)
 
 # Вывод результата
-print("Общие элементы:", intersection)
+print("Пересечение массивов:", result)
